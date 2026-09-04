95b661ac750a60bacffb25de693272049f061f91
akurd8560-ctrl <akurd8560@gmail.com>
2026-09-04 23:39:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 (() => {
   "use strict";
 
-  //const DEFAULTS = { rate: 1400, markup: 30, wa: };{
-  { "9647519099593" };
+  const DEFAULTS = { rate: 1400, markup: 30, wa:  "9647519099593" };
   const mem = {};
   const store = {
     get(k, d) {
